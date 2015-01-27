cb34127099ce6bcd9705f1ebdf2fe51693400753
leixu <leixu>
2015-01-27 06:17:48 +0000

    fix for mooncake 1. replace image url from cn.bing.com 2. replace music with LittleApple.mp3  #140 #141 #142

--- server/routes/image.js
+++ server/routes/image.js
@@ -7,7 +7,7 @@ exports.route = function(req, res) {
 
   //The url we want is: 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
   var options = {
-    host: 'www.bing.com',
+    host: 'cn.bing.com',
     path: '/HPImageArchive.aspx?format=js&idx=0&n=1'
   };
 
@@ -28,7 +28,7 @@ exports.route = function(req, res) {
         
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify({
-          url: 'http://www.bing.com' + url,
+          url: url,
           label: label
         }));
       } catch (error) {
